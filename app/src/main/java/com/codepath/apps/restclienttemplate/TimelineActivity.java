acea72bfa2d68ae8b32d473ba803ea3d458c8dc6
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2021-02-12 13:37:04 -0600

    Infinite Scroll

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -3,6 +3,7 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -24,6 +25,8 @@ public class TimelineActivity extends AppCompatActivity {
     RecyclerView rvTweets;
     List<Tweet> tweets;
     TweetsAdapter adapter;
+    SwipeRefreshLayout swipeContainer;
+    EndlessRecyclerViewScrollListener scrollListener;
 
 
     @Override
@@ -32,16 +35,60 @@ public class TimelineActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
 
          clinet = TwitterApp.getRestClient(this);
-
+        swipeContainer = findViewById(R.id.swipeContainer);
+        //configure refreshing color
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.i(TAG, "fetching new data");
+                populateHomeTimeline();
+            }
+        });
          //find the recycler view
         rvTweets = findViewById(R.id.rvTweets);
         //init the list of tweets and adapter
         tweets = new ArrayList<>();
         adapter = new TweetsAdapter(this, tweets);
         // recycler view setup: layout manager and the adapter
-        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rvTweets.setLayoutManager(layoutManager);
         rvTweets.setAdapter(adapter);
+
+
+        scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                loadMoreData();
+            Log.i(TAG, "onLoadMore" + page);
+            }
+        };
+        rvTweets.addOnScrollListener(scrollListener);
          populateHomeTimeline();
+    }
+
+    private void loadMoreData() {
+        clinet.getNextPageOfTweets(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "onSuccess for load more data" + json.toString() );
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    List<Tweet> tweets = Tweet.fromJsonArray(jsonArray);
+                    adapter.addAll(tweets);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.i(TAG, "onFailure for load more data" + throwable );
+            }
+        },tweets.get(tweets.size()-1).id);
     }
 
     private void populateHomeTimeline() {
@@ -51,8 +98,9 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.i(TAG, "OnSuccess" + json.toString());
                 JSONArray jsonArray = json.jsonArray;
                 try {
-                   tweets.addAll(Tweet.fromJsonArray(jsonArray));
-                   adapter.notifyDataSetChanged();
+                    adapter.clear();
+                    adapter.addAll(Tweet.fromJsonArray(jsonArray));
+                    swipeContainer.setRefreshing(false);
                 } catch (JSONException e) {
                     Log.e(TAG, "Json Exception", e);
                 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -52,7 +52,18 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
     public int getItemCount() {
         return tweets.size();
     }
+    // Clean all elements of the recycler
+    public void clear(){
+        tweets.clear();
+        notifyDataSetChanged();
+    }
 
+    // Add a list of items -- change to type used
+    public void addAll(List<Tweet> tweetlist)
+    {
+        tweets.addAll(tweetlist);
+        notifyDataSetChanged();
+    }
 
 
 
